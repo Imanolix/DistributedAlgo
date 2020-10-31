777c66bd98565119b9ad1ab55cd94eebcb3887c4
MUNTZ Imanol <ImanolixGaming@gmail.com>
2020-10-31 17:26:57 +0100

    Initial Project

---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,27 @@
+package com.example;
+
+import java.io.IOException;
+
+import com.example.Members;
+import com.example.Process;
+
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+
+//To print time
+import java.text.SimpleDateFormat;
+import java.util.Date;
+//
+
+import java.util.ArrayList;
+
+import java.util.Collections;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+
+
+
+}
---
+++ src/main/java/com/example/Process.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import akka.actor.UntypedAbstractActor;
+import akka.actor.ActorRef;
+import akka.actor.Props;
+
+import akka.event.Logging;
+import akka.event.LoggingAdapter;
+
+import java.util.ArrayList;
+
+public class Process {
+}
